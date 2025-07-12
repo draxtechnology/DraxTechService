83681f5c684b3529a8aa5d6b6bcc2d9d4452f964
Richard Jones <richard@binaryrefinery.com>
2025-07-12 11:33:12 +0100

    Just changed service name

--- Drax360NetworkManager/Properties/AssemblyInfo.cs
+++ Drax360NetworkManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Drax 360 NetworkManager")]
+[assembly: AssemblyTitle("Drax 360 Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Drax 360")]
-[assembly: AssemblyProduct("Drax 360 NetworkManager")]
+[assembly: AssemblyProduct("Drax 360 Service")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
